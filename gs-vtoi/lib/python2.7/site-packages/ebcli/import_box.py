5c5b21a691ccf181e0e0fffea66601e0b7003629
Sangyeob Lee <sy0414@gmail.com>
2016-04-14 14:26:01 +0900

    eb initiated

---
+++ gs-vtoi/lib/python2.7/site-packages/ebcli/import_box.py
@@ -0,0 +1,32 @@
+"""
+ElasticBox Confidential
+Copyright (c) 2014 All Right Reserved, ElasticBox Inc.
+
+NOTICE:  All information contained herein is, and remains the property
+of ElasticBox. The intellectual and technical concepts contained herein are
+proprietary and may be covered by U.S. and Foreign Patents, patents in process,
+and are protected by trade secret or copyright law. Dissemination of this
+information or reproduction of this material is strictly forbidden unless prior
+written permission is obtained from ElasticBox
+"""
+
+from ebcli import Boxes, AuthenticatedSession
+from ebcli import WORKSPACE_ID_ARG, BOXES_PATH_ARG, COMMENT_ARG, IMAGE_ARG, add_standard_argument
+
+
+def initialize_parser(subparsers):
+    parser = subparsers.add_parser('import', help='Import Box into a Workspace')
+
+    add_standard_argument(parser, BOXES_PATH_ARG)
+    add_standard_argument(parser, WORKSPACE_ID_ARG)
+    add_standard_argument(parser, COMMENT_ARG)
+    add_standard_argument(parser, IMAGE_ARG)
+    parser.add_argument(dest='box_name', metavar='box-name', help='Box ID')
+    parser.set_defaults(func=execute_command)
+
+
+def execute_command(args, _unknown):
+    session = AuthenticatedSession(args.url, args.token)
+
+    boxes = Boxes(args.path, session)
+    boxes.import_box(args.box_name, args.comment, args.workspace_id, args.image)
