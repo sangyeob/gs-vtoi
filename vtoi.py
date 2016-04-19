e2a66e0dc49d2234c8024a8ba2e787fbf3ea11be
Sangyeob Lee <sy0414@gmail.com>
2016-04-19 16:58:15 +0900

    resolve jpg size limit bug

--- vtoi.py
+++ vtoi.py
@@ -6,16 +6,18 @@ cap = imageio.get_reader("test_media/20160325_214851.mp4", 'ffmpeg')
 st = time.time()
 count = 0
 init = False
-for i, image in enumerate(cap):
-        if not init:
-                board = image
-                print image.shape[:2]
-                init = True
-        if count % 5 == 1:
-                board = numpy.hstack((board, image))
-        count += 1
-        print count,
+try:
+	for i, image in enumerate(cap):
+	        if not init:
+	                board = image
+	                print image.shape[:2]
+	                init = True
+	        if count % 5 == 1:
+	                board = numpy.hstack((board, image))
+	        count += 1
+	        print count,
+except:
+	pass
 print
+imageio.imwrite("static/img.tif", board)
 print 'elapsed time:', time.time() - st, '(s)'
-cv2.imwrite("a.png", board)
-cv2.imwrite("a.jpg", board)
